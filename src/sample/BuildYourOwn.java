430f0afcdcccb4f96841f1f4a262b6588015ceca
Shivam Patel <46139029+Shivam1615@users.noreply.github.com>
2020-04-11 14:07:08 -0400

    Update BuildYourOwn.java

--- src/sample/BuildYourOwn.java
+++ src/sample/BuildYourOwn.java
@@ -8,23 +8,29 @@ public class BuildYourOwn extends Pizza{
     private int MEDIUM_FLAT_FEE = 2;
     private int LARGE_FLAT_FEE = 4;
 
+
     public BuildYourOwn(String style, String size, ArrayList<String> toppings){
         super(style, size, toppings);
     }
 
     public int pizzaPrice() {
-        int price = SMALL_FLAT_FEE + (PRICE_PER_TOPPING * toppings.size());
-        if (size.equals("medium")) {
-            price += MEDIUM_FLAT_FEE; // + (PRICE_PER_TOPPING * toppings.size());
-        } else if (size.equals("large")) {
-            price += LARGE_FLAT_FEE; // + (PRICE_PER_TOPPING * toppings.size());
+        int price = 0;
+        if(toppings.size() < 1 || toppings.size() > 6){
+            throw new NullPointerException("Invaild number of toppings");
+        }else {
+            price = SMALL_FLAT_FEE + (PRICE_PER_TOPPING * toppings.size());
+            if (size.equals("medium")) {
+                price += MEDIUM_FLAT_FEE;
+            } else if (size.equals("large")) {
+                price += LARGE_FLAT_FEE;
+            }
         }
         return price;
     }
 
     @Override
     public String toString() {
-        return this.style + " " + this.size.toUpperCase() + "\nToppings: " +
+        return this.style + ":" + this.size.toUpperCase() + "\nToppings: " +
                 this.toppings.toString().replace("[", "").replace("]", "")
                 + "\nPrice: $" + pizzaPrice() + "\n";
     }
